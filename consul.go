5086351888541618c5b9a1ed4048be19ef622d8b
jbdalido <jbdalido@gmail.com>
2015-03-17 18:18:11 +0100

    add skeleton interfaces for etcd and consul

--- consul.go
+++ consul.go
@@ -1,1 +1,29 @@
 package meechum
+
+type Consul struct {
+	Host string
+}
+
+func (c *Consul) SetKey(key string, value interface{}) error {
+
+}
+
+func (c *Consul) SetTtlKey(key string, value interface{}, ttl int) error {
+
+}
+
+func (c *Consul) GetKey(key string) (interface{}, error) {
+
+}
+
+func (c *Consul) ListDirectory(dir string) (interface{}, error) {
+
+}
+
+func (c *Consul) DeleteKey(key string) error {
+
+}
+
+func (c *Consul) UpdateKey(key string, value interface{}) error {
+
+}
--- etcd.go
+++ etcd.go
@@ -1,3 +1,29 @@
 package meechum
 
-// HANDLER ETCD
+type Etcd struct {
+	Host string
+}
+
+func (e *Etcd) SetKey(key string, value interface{}) error {
+
+}
+
+func (e *Etcd) SetTtlKey(key string, value interface{}, ttl int) error {
+
+}
+
+func (e *Etcd) GetKey(key string) (interface{}, error) {
+
+}
+
+func (e *Etcd) ListDirectory(dir string) (interface{}, error) {
+
+}
+
+func (e *Etcd) DeleteKey(key string) error {
+
+}
+
+func (e *Etcd) UpdateKey(key string, value interface{}) error {
+
+}
